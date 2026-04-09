ae200897630eb30711dedbf549b553b081aca005
MonarkGoyal <monark.goyal19@gmail.com>
2026-04-10 02:10:56 +0530

    Use whitelogo for favicon

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -2,6 +2,20 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import whiteLogo from './assets/whitelogo.jpg'
+
+const setFavicon = () => {
+  let link = document.querySelector("link[rel~='icon']")
+  if (!link) {
+    link = document.createElement('link')
+    link.rel = 'icon'
+    document.head.appendChild(link)
+  }
+  link.type = 'image/jpeg'
+  link.href = whiteLogo
+}
+
+setFavicon()
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
