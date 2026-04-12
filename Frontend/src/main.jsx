b2534c366d91ae1f3eff953b56854bbf519ce229
MonarkGoyal <monark.goyal19@gmail.com>
2026-04-12 23:29:15 +0530

    chore: update favicon to ArkGPT logo and remove OpenAI logos

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -3,20 +3,6 @@ import { createRoot } from 'react-dom/client'
 import '@fortawesome/fontawesome-free/css/all.min.css'
 import './index.css'
 import App from './App.jsx'
-import whiteLogo from './assets/whitelogo.jpg'
-
-const setFavicon = () => {
-  let link = document.querySelector("link[rel~='icon']")
-  if (!link) {
-    link = document.createElement('link')
-    link.rel = 'icon'
-    document.head.appendChild(link)
-  }
-  link.type = 'image/jpeg'
-  link.href = whiteLogo
-}
-
-setFavicon()
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
